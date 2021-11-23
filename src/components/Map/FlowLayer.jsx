5e0e52ef0f1495421b1bb4a4a7b7e84829378865
zyq6 <zyq6@meitu.com>
2021-11-23 23:16:08 +0800

    feat: 需求现改

--- src/components/Map/FlowLayer.jsx
+++ src/components/Map/FlowLayer.jsx
@@ -1,34 +1,19 @@
-import { ImageLayer, Text } from '@amap/amap-vue';
-import Mask from './Mask';
+import { ImageLayer } from '@amap/amap-vue';
 
 import layerFlow from '@/assets/MapPlugin/layer-flow.png';
-import { CENTER } from '@/constants';
 
 export default {
   name: 'FlowLayer',
 
   render() {
     return (
-      <div>
-        <Text
-          position={CENTER}
-          text="大樟溪流域"
-          offset={[-200, -100]}
-          domStyle={{
-            color: 'rgba(0, 120, 255, 0.3)',
-            fontSize: '64px',
-            letterSpacing: '20px'
-          }}
-        />
-        <Mask />
-        <ImageLayer
-          url={layerFlow}
-          bounds={[
-            [118.37484643554688, 25.62028620774471],
-            [119.22009926757814, 26.111021607777472]
-          ]}
-        />
-      </div>
+      <ImageLayer
+        url={layerFlow}
+        bounds={[
+          [118.37484643554688, 25.62028620774471],
+          [119.22009926757814, 26.111021607777472]
+        ]}
+      />
     );
   }
 };
