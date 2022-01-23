7d42eed91fe64a18368181974652ca88bd43c4b7
zyq6 <zyq6@meitu.com>
2022-01-23 22:48:22 +0800

    feat: 改来改去

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,12 @@
-import YtMap from '@/components/YtMap';
-import { SatelliteLayer, Polygon, Marker } from '@amap/amap-vue';
+// import YtMap from '@/components/YtMap';
+// import { StreetsPolygon } from '@/components/Map';
 import SideBar from './SideBar';
-import { Carousel, CarouselItem } from 'element-ui';
 import Header from './Header';
 import styles from './Home.module.less';
 
-import { FUZHOU, YONGTAI } from '@/constants';
-import { coordinates } from '@/assets/Geo/Yongtai.json';
-import markerBlue from '@/assets/MapPlugin/marker-blue.png';
-import markerRed from '@/assets/MapPlugin/marker-red.png';
-import bg0 from '@/assets/Bg/home1.jpeg';
-import bg1 from '@/assets/Bg/home2.jpeg';
-import bg2 from '@/assets/Bg/home3.jpeg';
-import bg3 from '@/assets/Bg/home4.jpeg';
-// import bg4 from '@/assets/Bg/home5.jpeg';
-import title from '@/assets/Bg/bg-title.png';
+import Title from '@/assets/Bg/home-title.png';
+import HeaderBg from '@/assets/Bg/home-header-bg.png';
+import Names from '@/assets/Bg/home-streets-name.png';
 
 export default {
   name: 'Home',
@@ -28,68 +20,13 @@ export default {
   render() {
     return (
       <div class={styles.home}>
-        <YtMap zoom={9} showLabel={false}>
-          <Header />
-          <SatelliteLayer visible />
-
-          <Polygon
-            path={coordinates}
-            strokeColor="#0078FF"
-            fillColor="#0078FF"
-            fillOpacity={0.2}
-          />
-
-          <Marker
-            position={FUZHOU}
-            icon={markerRed}
-            label={{
-              content: '福州市区'
-            }}
-            class={styles.markerRed}
-          />
-
-          <Marker
-            position={YONGTAI}
-            icon={markerBlue}
-            label={{
-              content: '永泰县'
-            }}
-          />
-        </YtMap>
-
-        <Carousel type="card" direction="vertical">
-          <CarouselItem key="bg0">
-            <div
-              class={styles.carouselItem}
-              style={{ backgroundImage: `url(${bg0})` }}
-              onClick={this.open.bind(this, '/profile/ecological')}
-            ></div>
-          </CarouselItem>
-          <CarouselItem key="bg1">
-            <div
-              class={styles.carouselItem}
-              style={{ backgroundImage: `url(${bg1})` }}
-              onClick={this.open.bind(this, '/profile/industrial')}
-            ></div>
-          </CarouselItem>
-          <CarouselItem key="bg2">
-            <div
-              class={styles.carouselItem}
-              style={{ backgroundImage: `url(${bg2})` }}
-              onClick={this.open.bind(this, '/profile/urban')}
-            ></div>
-          </CarouselItem>
-          <CarouselItem key="bg3">
-            <div
-              class={styles.carouselItem}
-              style={{ backgroundImage: `url(${bg3})` }}
-              onClick={this.open.bind(this, '/profile/livelihood')}
-            ></div>
-          </CarouselItem>
-        </Carousel>
-
-        <img src={title} class={styles.title} />
-
+        <Header />
+        <img src={Title} />
+        <img src={HeaderBg} />
+        <img src={Names} class={styles.streetsName} />
+        {/* <YtMap showLabel={false}>
+          <StreetsPolygon />
+        </YtMap> */}
         <SideBar />
       </div>
     );
